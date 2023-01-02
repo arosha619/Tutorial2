995f1467bf5c1ac51d841e1a780819288b6ffbc3
arosha619 <90087992+arosha619@users.noreply.github.com>
2023-01-02 07:29:36 +0530

    first

---
+++ src/pages/page-2.js
@@ -0,0 +1,33 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout/layout"
+import Seo from "../components/seo"
+
+const SecondPage = () => (
+  <Layout>
+    <h1>Hi from the second page</h1>
+    <p>Welcome to page 2</p>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+export const Head = () => <Seo title="Page two" />
+export default SecondPage
+
+// import * as React from "react"
+// import { Link } from "gatsby"
+
+// import Layout from "../components/layout/layout"
+// import Seo from "../components/seo"
+
+// const SecondPage = () => (
+//   <Layout>
+//     <h1>Hi from the second page</h1>
+//     <p>Welcome to page 2</p>
+//     <Link to="/">Go back to the homepage</Link>
+//   </Layout>
+// )
+
+// export const Head = () => <Seo title="Page two" />
+
+// export default SecondPage
